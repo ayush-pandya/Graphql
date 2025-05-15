b7421cfce40c9e5247273ab3f83a52fe263d1f50
Ayush <ayushpandya517@gmail.com>
2025-05-15 14:51:46 +0530

    add resolver

--- graphql/resolver.go
+++ graphql/resolver.go
@@ -4,4 +4,25 @@ package graphql
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+import (
+	"context"
+)
+
+// Database interface defines methods our resolvers need
+type Database interface {
+	CreateTicket(ctx context.Context, ticket *Ticket) error
+	GetTicket(ctx context.Context, id string) (*Ticket, error)
+	// Add other database methods as needed
+}
+
+// Resolver holds dependencies that will be used across resolvers
+type Resolver struct {
+	db Database
+}
+
+// NewResolver creates a new Resolver with the required dependencies
+func NewResolver(db Database) *Resolver {
+	return &Resolver{
+		db: db,
+	}
+}
